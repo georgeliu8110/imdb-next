185d0611991b358096467e2a9b5ab95dcf682a67
George Liu <georgeliu8110@gmail.com>
2024-02-01 00:16:43 -0600

    add home link for Imdb icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import MenuItem from "./MenuItem";
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
+import Link from 'next/link'
 
 export default function Header() {
   return(
@@ -9,10 +10,10 @@ export default function Header() {
         <MenuItem title="home" address="/" Icon={AiFillHome}/>
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill}/>
       </div>
-      <div className='flex gap-1 items-center'>
+      <Link href={'/'} className='flex gap-1 items-center'>
         <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
         <span className='text-xl hidden sm:inline'>Clone</span>
-      </div>
+      </Link>
     </div>
   )
 }
