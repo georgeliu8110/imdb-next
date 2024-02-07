b23687dfeab42cfd78b82c8dea57813aa7bc68b3
George Liu <georgeliu8110@gmail.com>
2024-02-06 22:58:28 -0600

    add Suspense component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import NavbarItem from './NavbarItem'
+import {Suspense} from 'react'
 
 export default function Navbar() {
   return (
     <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-4'>
+      <Suspense fallback={<div>Loading...</div>}>
       <NavbarItem title='Trending' param='fetchTrending'/>
       <NavbarItem title='Top Rated' param='fetchTopRated'/>
+      </Suspense>
     </div>
   )
 }
